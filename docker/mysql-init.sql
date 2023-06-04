7c45f31df2404b8af4ba3251ebb5a5ade112ace5
mcdulltii <mcdulltii@gmail.com>
2023-06-04 10:34:42 -0400

    Added foreign key to users table

--- docker/mysql-init.sql
+++ docker/mysql-init.sql
@@ -1,13 +1,16 @@
-CREATE TABLE `users` (
-  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE `forums` (
   `forum_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `forum_name` VARCHAR(255) NOT NULL,
   `forum_category` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`forum_id`),
+  PRIMARY KEY (`forum_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `forum_id` INT UNSIGNED NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`forum_id`) REFERENCES `forums`(`forum_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
