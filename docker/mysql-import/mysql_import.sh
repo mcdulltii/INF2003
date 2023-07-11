537b2d0a82d32dda04e405ed228dba9a641ca0d3
Aaron <mcdulltii@gmail.com>
2023-07-11 15:45:28 +0000

    Removed redundant files

--- docker/mysql-import/mysql_import.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-docker exec -i mariadb sh -c 'exec mariadb -uuser -ppassword mysql_db < /forums.csv'
-docker exec -i mariadb sh -c 'exec mariadb -uuser -ppassword mysql_db < /posts.csv'
-docker exec -i mariadb sh -c 'exec mariadb -uuser -ppassword mysql_db < /users.csv'
-docker exec -i mariadb sh -c 'exec mariadb -uuser -ppassword mysql_db < /votes.csv'
